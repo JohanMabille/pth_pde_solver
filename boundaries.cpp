4fb884f196099902f109f0c8abdfcf5cc73459e6
TheophileCecillon <theophile.12@hotmail.fr>
2019-01-24 19:50:19 +0100

    implementation of the boundary conditions

--- boundaries.cpp
+++ boundaries.cpp
@@ -4,31 +4,32 @@
 
 boundary::boundary(std::string name)
 :b_name(name)
-	{
-	}
+{
+}
 
 
+dirichlet::dirichlet(option option, std::vector<double> spot, double N)
+:boundary("Dirichlet")
+{
+    up_bd = option.get_payoff(spot[N-1]);
+    down_bd = 0;
+}
 
-dirichilet::dirichilet(option option, std::vector<double> spot, double N):boundary("Dirichilet")
-        {
-            up_bd=option.get_payoff(spot[N-1]);
-            down_bd=0;
-        }
-
-neumann::neumann(option option, std::vector<double> spot, double N):boundary("Neumann")
+neumann::neumann(option option, std::vector<double> spot, double N)
+:boundary("Neumann")
 {
     // Modifier pour adapter aux conditions aux bornes de Neumann
-    up_bd=option.get_payoff(spot[N-1]);
-    down_bd=0;
+    up_bd = option.get_payoff(spot[N-1]);
+    down_bd = 0;
 }
 
 
 double boundary::get_up_bd()
-    {
-        return up_bd;
-    }
+{
+    return up_bd;
+}
 
 double boundary::get_down_bd()
-    {
-        return down_bd;
-    }
+{
+    return down_bd;
+}
--- boundaries.hpp
+++ boundaries.hpp
@@ -7,36 +7,39 @@
 
 class boundary
 {
-	//s
-	public:
-		boundary(std::string name);
-        std::string b_name;
-        double get_up_bd();
-        double get_down_bd();
-    protected:
-        double up_bd;
-        double down_bd;
-
+	
+public:
+    boundary(std::string name);
+    std::string b_name;
+    double get_up_bd();
+    double get_down_bd();
+    
+protected:
+    double up_bd;
+    double down_bd;
 };
 
-	class dirichilet : public boundary
-	{
-	public:
-		dirichilet(option option, std::vector<double> spot, double N);
 
-	private:
-        // Ajouter input propres à Dirichilet
-	};
+class dirichlet : public boundary
+{
+    
+public:
+    dirichlet(option option, std::vector<double> spot, double N);
+private:
+    // Ajouter input propres à Dirichlet
+};
 
-	class neumann : public boundary
-	{
-	public:
-        neumann(option option, std::vector<double> spot, double N);
-	private:
-        // Ajouter input propres à Neumann
-	};
 
-	//Add another derived class to specify the wanted boundary condition
+class neumann : public boundary
+{
+    
+public:
+    neumann(option option, std::vector<double> spot, double N);
+private:
+    // Ajouter input propres à Neumann
+};
+
+// Add another derived class to specify the wanted boundary condition
 
 
 #endif /* boundaries_hpp */
