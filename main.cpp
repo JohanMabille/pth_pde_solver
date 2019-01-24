dfcefbe326a9ffedac4f851bfc263677b32914ed
hannahbruck <hannah.bruck@outlook.com>
2019-01-24 16:28:46 +0100

    Debug Hannah

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
-#include "projet.hpp"
-#include "solver_tridiag.hpp"
-#include <math.h>
-#include "volatility.hpp"
-#include "rates.hpp"
 #include "option.hpp"
+#include "projet.hpp"
+#include "rates.hpp"
+#include "solver_tridiag.hpp"
+#include "volatility.hpp"
+#include <math.h>
 
 
 namespace initialisation
 {
- 
-    /////////// payoff that can be modified by user ///////////////
-  
-    
-    double payoff(double spot,double strike)
-    {
-        return std::max(spot-strike,0.);
-    }
     
     double dx(double sigma,double maturity,double N)
     {
@@ -24,7 +16,7 @@ namespace initialisation
     
     double dt(double maturity)
     {
-        return 1.0/365.0;
+        return 1.0/maturity;
     }
     
     
@@ -47,39 +39,28 @@ int main(int artc, char* argv[])
     string type_rate = "bs_rates";
     
     double spot=100.;
-    double strike =100. ;
+    double strike =100.;
     double maturity=365.0;
-    double volatility = 0.10  ;
-    double r = 0.01 ;
+    double T=1;
+    double volatility = 0.10;
+    double r = 0.01;
     double theta = 0.5;
-    double theta_stochastic = 0.5 ;
-    double N=100. ;
+    double theta_stochastic = 0.5;
+    double N=1000.;
     double kappa = 0.5  ;
-    double dt = initialisation::dt(maturity)  ;
-    double dx = initialisation::dx(volatility,maturity,N)  ;
-    std::vector<double> Spots  = initialisation::Spots(spot,volatility,maturity,N) ;
-
-        bs_vol vol(volatility,maturity) ;
-        bs_rates rates(r,maturity)  ;
-   
-        vanilla chosen_option("call",strike,maturity) ;
+    double dt = initialisation::dt(maturity);
+    double dx = initialisation::dx(volatility,T,N);
+    std::vector<double> Spots  = initialisation::Spots(spot,volatility,T,N);
     
-      std::vector<double> vol_vect = vol.get_vector()  ;
-      std::vector<double> rates_vect = rates.get_vector()  ;
-      std::cout << "yo" << std::endl;
-   // std::vector<double> result = solver_price(chosen_option,Spots,maturity,N,theta,vol_vect,rates_vect,dt,dx)  ;
-  //  std::cout << result[3] << std::endl;
-
-    double rate = rates_vect[60];
-    double vola = vol_vect[60];
-    double alpha = variables::omega(theta-1.,dt,vola,dx,rate);
-    std::cout << alpha << std::endl;
- 
+    bs_vol vol(volatility,maturity);
+    bs_rates rates(r,maturity);
+    vanilla chosen_option("call",strike,maturity);
+    
+    std::vector<double> vol_vect = vol.get_vector();
+    std::vector<double> rates_vect = rates.get_vector();
+    std::vector<double> result = solver_price(chosen_option,Spots,maturity,N,theta,vol_vect,rates_vect,dt,dx);
+    
+    std::cout << "Current option price (from our solver) is "  << result[N/2] << " euros" << std::endl;
     
 }
-
-
-
-// pb avec type option n'appelle pas le bon get_payoff 
-
 
--- projet.cpp
+++ projet.cpp
@@ -1,61 +1,59 @@
-#include "projet.hpp"
-#include "solver_tridiag.hpp"
 #include "option.hpp"
+#include "projet.hpp"
+#include "rates.hpp"
+#include "solver_tridiag.hpp"
+#include "volatility.hpp"
 #include <iostream>
 using namespace std;
 #include <vector>
-#include "volatility.hpp"
-#include "rates.hpp"
 #include <math.h>
 
-// maturity in years
 
 std::vector<double> solver_price(vanilla option, std::vector<double> Spots, double T, double N, double theta, std::vector<double> sigma, std::vector<double> r, double dt, double dx) //
-    {
-        std::vector<double> f = init_vectors::vector_f(option, N, Spots);
-        f[0] = 0;      // conditions aux bornes
-        f[N-1] = option.get_payoff(Spots[N-1]);
-        
+{
+    std::vector<double> f = init_vectors::vector_f(option, N, Spots);
+    f[0] = 0;      // conditions aux bornes
+    f[N-1] = option.get_payoff(Spots[N-1]);
     
-        std::vector<double> x(N);
+    std::vector<double> x(N);
+    
+    //loop that goes backward, from maturity to today
+    for(int i=(T-1); i>=0; i--)
+    {
+        double rate = r[i];
+        double vol = sigma[i];
+        double alpha = variables::alpha(theta-1,dt,vol,dx,rate);   // alpha(theta-1)
+        double beta = variables::beta(theta-1,dt,vol,dx,rate);   // beta(theta-1)
+        double omega = variables::omega(theta-1,dt,vol,dx,rate);     // omega(theta-1)
         
-        //loop that goes backward, from maturity to today
-        for(int i=(T-1); i>=0; i--)
+        //We build the right member of the equation --> M(theta-1)*Fn
+        std::vector<double> d(N);
+        d[0] = beta*f[0]+omega*f[1];
+        d[N-1] = alpha*f[N-2]+beta*f[N-1];
+        for(int j=1; j<N-1; j++)
         {
-            double rate = r[i];
-            double vol = sigma[i];
-            double alpha = variables::alpha(theta-1,dt,vol,dx,rate);   // alpha(theta-1)
-            double beta = variables::beta(theta-1,dt,vol,dx,rate);   // beta(theta-1)
-            double omega = variables::omega(theta-1,dt,vol,dx,rate);     // omega(theta-1)
-
-            //We build the right member of the equation --> M(theta-1)*Fn
-            std::vector<double> d(N);
-            d[0] = beta*f[0]+omega*f[1];
-            d[N-1] = alpha*f[N-2]+beta*f[N-1];
-             for(int j=1; i<N-1; i++)
-             {
-                 d[j] = alpha*f[j-1]+beta*f[j]+omega*f[j+1];
-             }
-            std::vector<double> a = init_vectors::vector_a(N,theta,dt,vol,dx,rate); //vector under the diagonal
-            std::vector<double> b = init_vectors::vector_b(N,theta,dt,vol,dx,rate); // diagonal vector
-            std::vector<double> c = init_vectors::vector_c(N,theta,dt,vol,dx,rate); //vector above the diagonal
-            
-             x = solvers::solver_trid(a,b,c,d);    // we resolve using the tridiagonal solver
-            
-            if (i==0)
-            {
-                return x;  //we return the vector of option price if we are at the last step
-            }
-            else
-            {
-                f = x;
-                f[0] = 0;
-                f[N-1] = Spots[N-1]-option.get_strike();
-            }
-
+            d[j] = alpha*f[j-1]+beta*f[j]+omega*f[j+1];
         }
-        return x ;
+        std::vector<double> a = init_vectors::vector_a(N,theta,dt,vol,dx,rate); //vector under the diagonal
+        std::vector<double> b = init_vectors::vector_b(N,theta,dt,vol,dx,rate); // diagonal vector
+        std::vector<double> c = init_vectors::vector_c(N,theta,dt,vol,dx,rate); //vector above the diagonal
+        
+        x = solvers::solver_trid(a,b,c,d);    // we resolve using the tridiagonal solver
+        
+        if (i==0)
+        {
+            return x;  //we return the vector of option price if we are at the last step
+        }
+        else
+        {
+            f = x;
+            f[0] = 0;
+            f[N-1] = option.get_payoff(Spots[N-1]);
+        }
+        
     }
+    return x ;
+}
 
 namespace init_vectors
 {
@@ -73,9 +71,9 @@ namespace init_vectors
     std::vector<double> vector_a(double N, double theta, double dt, double sigma, double dx, double r)  //vector under the diagonal
     {
         std::vector<double> a(N-1);
-        for(int i=0; i<(N-2); i++)
+        for(int i=0; i<(N-1); i++)
         {
-            a[i]=variables::alpha( theta,dt,sigma,dx,r);
+            a[i]=variables::alpha(theta,dt,sigma,dx,r);
         }
         return a;
     }
@@ -83,7 +81,7 @@ namespace init_vectors
     std::vector<double> vector_b(double N, double theta, double dt, double sigma, double dx, double r)   //diagonal vector
     {
         std::vector<double> b(N);
-        for(int i=0; i<(N-1); i++)
+        for(int i=0; i<N-1; i++)
         {
             b[i]=variables::beta(theta,dt,sigma,dx,r);
         }
@@ -93,7 +91,7 @@ namespace init_vectors
     std::vector<double> vector_c(double N, double theta, double dt, double sigma, double dx, double r)   // vector above the diagonal
     {
         std::vector<double> c(N-1);
-        for(int i=0; i<(N-2); i++)
+        for(int i=0; i<(N-1); i++)
         {
             c[i]=variables::omega(theta,dt,sigma,dx,r);
         }
@@ -106,26 +104,19 @@ namespace variables
 {
     double alpha(double theta, double dt, double sigma, double dx, double r)
     {
-        double temp = -0.5*theta*dt*pow(sigma/dx,2)+0.25*theta*dt*(r-pow(sigma,2))/dx;
+        double temp = -0.5*theta*dt*pow(sigma/dx,2.) + 0.25*theta*dt*(r-pow(sigma,2.))/dx;
         return temp;
     }
     
     double beta(double theta, double dt, double sigma, double dx, double r)
     {
-        double temp = 1 + theta*dt*pow(sigma/dx,2) + theta*dt*r;
+        double temp = 1. + theta*dt*pow(sigma/dx,2.) + theta*dt*r;
         return temp;
     }
     
     double omega(double theta, double dt, double sigma, double dx, double r)
     {
-        double temp = 0.5*theta*dt*(-pow(sigma/dx,2)) + 0.25*theta*(pow(sigma,2)-r)/dx;
+        double temp = 0.5*theta*dt*(-pow(sigma/dx,2.)) + 0.25*theta*dt*(pow(sigma,2.)-r)/dx;
         return temp;
     }
 }
-
-
-void print_solver_price(std::vector<double> x, double N)
-{
-        std::cout << "Current option price is (from our solver) is"  << x[N/2] << " euros" << std::endl;
-}
-
