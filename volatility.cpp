482bfaf02c93f31afc96325ae3901cbdace2cc8f
TheophileCecillon <theophile.12@hotmail.fr>
2019-01-25 15:38:39 +0100

    modification of heston, cir models and normal_law

--- volatility.cpp
+++ volatility.cpp
@@ -4,28 +4,30 @@
 using namespace std;
 #include <vector>
 #include <math.h>
+#include <time.h>
 
 volatility::volatility(std::string name)
 :v_name(name)
 {
 }
 
-bs_vol::bs_vol(double vol, double T)
+bs_vol::bs_vol(double vol, double maturity)
 :volatility("Black-Scholes")
 {
-    vol_vector.resize(T);
-    for(int i=0; i<T-1; i++)
+    vol_vector.resize(maturity);
+    for(int i=0; i<maturity-1; i++)
     {
         vol_vector[i] = vol;
     }
 }
 
-heston_vol::heston_vol(double vol, double T, double dt, double kappa, double theta)
+heston_vol::heston_vol(double vol, double maturity, double dt, double kappa, double theta)
 :volatility("Heston")
 {
-    vol_vector.resize(T);
+    vol_vector.resize(maturity);
     vol_vector[0] = vol;
-    for(int i=1; i<T-1; i++)
+    srand(time(NULL));
+    for(int i=1; i<maturity-1; i++)
     {
         vol_vector[i] = vol_vector[i-1] + kappa*(theta-fmax(vol_vector[i-1],0))*dt + vol*sqrt(fmax(vol_vector[i-1],0))*sqrt(dt)*normal_law(3);
     }
